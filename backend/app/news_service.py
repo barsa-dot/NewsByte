42829824ce5270c156ab45bf60b9df8c68c69333
barsa-dot <UGCET2502471@reva.edu.in>
2026-06-23 21:01:49 +0530

    Integrate NewsAPI service

--- backend/app/news_service.py
+++ backend/app/news_service.py
@@ -1,4 +1,15 @@
 import requests
+from app.config import NEWS_API_KEY
 
 def fetch_news():
+    url = (
+        f"https://newsapi.org/v2/top-headlines?"
+        f"country=us&apiKey={NEWS_API_KEY}"
+    )
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return response.json()["articles"]
+
     return []
